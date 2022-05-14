e9fce585878569cea5e17f1081c974b51e8bee09
david-gonzalez-coder <david-gonzalez-admin@protonmail.com>
2022-05-13 21:04:04 -0500

    refactor atomicClass

--- src/App.jsx
+++ src/App.jsx
@@ -16,15 +16,25 @@ createTheme({
 })
 
 function App() {
+  const regexp = /t(e)(st(\d?))/g;
+  const str = 'test1test2';
+  
+  const array = [...str.matchAll(regexp)];
+  
 
-console.log(Theme)
- 
+  // expected output: Array ["test1", "e", "st1", "1"]
+  
 
   return (
     <>
-     <Box h="100px" rounded width="100px" shading   m="10px"bg="#f2f2f2"></Box>
-     <Box h="100px" rounded width="100px"  boxShadow="1px 2px 3px #ccc"  m="10px" bg="#f2f2f2"></Box>
-     <Box atomicClass="h:100px w:100px m:10px bg:#f2f2f2" rounded shading></Box>
+     {/* <Box sqr="100px 10px 20px * #f2f2f2 flex" center>asd</Box> */}
+     {/* <Box atomicClass="bg:red" breakpoints="sm{bg:pink} lg{bg:orange}">hola</Box>
+     <Box bg="#ccc" h="20px" myStyle={{br: '10px'}}></Box>
+     <Box breakpoints="sm{bg:pink} md{bg:blue}" atomicClass="bg:red">hola</Box> */}
+      <Box atomicClass="h:100px w:70vw br:10px .cssClass{h:50px;bg:#ccc}" shading>
+        <div className="cssClass"></div>
+      </Box>
+     
     </> 
   )
 }
